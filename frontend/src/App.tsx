b136707511ecb9663174ac6df31b69cdd84a4754
Jean Carlos <jeancarlos272010@gmail.com>
2022-07-14 16:20:48 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 import NotificationButton from "./components/NotificationButton" //exporta a função do arquivo index.tsx do notificationButton
 //componente é um pedaço de codigo que pode ser reaproveitado
+//main fica sendo a estrutura da página
 function App() {
   return ( //<> serve para retornar mais de uma tag
     <>
-      <h1>Olá mundo!</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
