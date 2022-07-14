c909a493668c57f6b631d0e0738796ced26ae2ef
Jean Carlos <jeancarlos272010@gmail.com>
2022-07-14 15:23:38 -0300

    Project Clean

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render( //mostra todo conteudo do main atraves do root em index.html
+  //App.txs
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 )
